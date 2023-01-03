3405a53edba958fb62fb373764f4e010083ef076
carvajalmaxi <carvajalmaxi1@gmail.com>
2023-01-02 21:34:15 -0300

    Version 1.0

---
+++ app.js
@@ -0,0 +1,25 @@
+const path =  require("path")
+const express = require("express");
+const res = require("express/lib/response");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Se prendió en el puerto ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/home.html"))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/register.html"))
+});
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/login.html"))
+});
+
